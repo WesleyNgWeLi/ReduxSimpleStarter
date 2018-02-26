b01d8a3652c482ddc282ed64a035673d1847782b
wesleyngweli <wesleyngweli@gmail.com>
2018-02-25 23:11:30 -0600

    Pre sleep push

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
 
 
 const API_KEY = 'AIzaSyDTc_yFeIGebHQG8PBCwkwvRiBCEb-pgLo'
 
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+	console.log(data);
+})
 
 
 // Create a new component. 
 //This component should produce some HTML
-const App =()=> {
-    return (
-        <div>
-            <SearchBar> </SearchBar>
-        </div>
-    )
+class App extends Component {
+    render () {
+    	return (        
+			<div>
+	            <SearchBar> </SearchBar>
+	        </div>
+        );
+    }
 }
 
 // Take this components 
