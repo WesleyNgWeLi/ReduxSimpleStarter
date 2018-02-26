93807fd14db60d931f4657f3547b1ad9abf9f9aa
Wesley Ng <wesleyngweli@gmail.com>
2018-02-26 13:34:18 -0600

    Session 2, 23 push

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -13,7 +13,6 @@ class SearchBar extends Component {
                 <input 
                     value = {this.state.term}
                     onChange={event => this.setState({ term: event.target.value})} />
-                    Value of the input: {this.state.term}
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -3,27 +3,33 @@ import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
 
 
 const API_KEY = 'AIzaSyDTc_yFeIGebHQG8PBCwkwvRiBCEb-pgLo'
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-	console.log(data);
-})
-
-
-// Create a new component. 
-//This component should produce some HTML
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({videos});
+            //this.setState({videos: videos}) only works when the key and prop is the same
+        })
+    }
+
     render () {
     	return (        
 			<div>
-	            <SearchBar> </SearchBar>
+	            <SearchBar/>
+                <VideoList videos={this.state.videos} />
 	        </div>
         );
     }
 }
 
-// Take this components 
+// Take this component's
 //generated HTML and put it on the page/in the DOM
 ReactDOM.render(<App />, document.querySelector('.container'))
